ddf930dff577e16b0c1a969afa5dd3e6c8dac8ec
hyo00joo <hyoeun.jin2@gmail.com>
2024-06-18 21:35:42 +0900

    과제

--- c/string/string.c
+++ c/string/string.c
@@ -10,20 +10,22 @@ long long strlen(char * const str){
 }
 
 int strcmp(char* const str1, char* const str2){
-    
-    if(str1 == str2)
-        return 0;
-    if(str1 > str2)
-        return -1;
-    if(str1 < str2)
-        return 1;
-    
+    int i = 0;
+    while (str1[i] != '\0' || str2[i] != '\0') {
+        if (str1[i] < str2[i])
+            return -1;
+          else if (str1[i] > str2[i])
+            return 1;
+         i++;
+    }
+    return 0;
 }
 
-int *strcpy(char* const source, char* destination);
-
+int *strcpy(char* const source, char* destination)
+{
     destination = source;
-    
+    return 0;
+}
     
 
 
