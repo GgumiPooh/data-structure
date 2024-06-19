3581e9847fea462a3ddb8d421ea18c0be086db4d
Jehee Cheon <jeheecheon@gmail.com>
2024-06-19 11:17:23 +0900

    과제: struct swapping

---
+++ c/assignments/pointer/structswap.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+typedef struct {
+    int weight;
+    int height;
+} Human;
+
+void swap(Human* h1, Human* h2) {
+
+}
+
+int main(void) {
+    Human child = {
+        20, 120
+    };
+    Human adult = {
+        30, 333
+    };
+
+    printf("Before Swapped\n");
+    printf("child\nchild's weight: %d, child's height: %d\n", child.weight, child.height);
+
+    // swap(child, adult);
+
+    printf("Before Swapped\n");
+    printf("adult\nadult's weight: %d, adult's height: %d\n", adult.weight, adult.height);
+
+    return 0;
+}
