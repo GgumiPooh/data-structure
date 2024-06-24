da74be0b3de56359c74c08a4ab6ddaea23613ebe
hyo00joo <hyoeun.jin2@gmail.com>
2024-06-24 16:19:56 +0900

    ?

--- c/linked-list/main.c
+++ c/linked-list/main.c
@@ -4,8 +4,9 @@
 int main(void) {
     Node* head = create_node(5);
     append_node(&head, 4);
-    insert_node(&head, 6, 1);
-    delete_node(&head, 1);
+    append_node(&head, 3);
+    insert_node(&head, 6, 3);
+    //delete_node(&head, 0);
     
     print_list(head);
 
