4c10b6ae08b299794d4b91bcfdf52ffb27cd3ade
jeheecheon <jeheecheon@gmail.com>
2024-06-28 01:12:02 +0900

    과제: 계산기 (pointer-2 폴더)

---
+++ c/assignments/pointer-2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// + 더하기 연산
+void sum(int x, int y, int *result);
+
+// - 빼기 연산
+void minus(int x, int y, int *result);
+
+// * 곱하기 연산
+void multiply(int x, int y, int *result);
+
+// / 나누기 연산
+void divide(int x, int y, int *result);
+
+// % 나머지 연산
+void mod(int x, int y, int *result);
+
+int main(void) {
+    int result;
+
+    sum(1, 2, &result);
+    printf("%d\n", result);
+
+    minus(1, 2, &result);
+    printf("%d\n", result);
+
+    multiply(1, 2, &result);
+    printf("%d\n", result);
+
+    divide(1, 2, &result);
+    printf("%d\n", result);
+
+    mod(1, 2, &result);
+    printf("%d\n", result);
+
+    return 0;
+}
